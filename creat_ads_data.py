d1d05a9930ae499df3ad0e7d19faa1f66da5d5cf
cassie1103 <cassie1103@163.com>
2016-09-18 13:11:41 +0800

    update

--- creat_ads_data.py
+++ creat_ads_data.py
@@ -9,9 +9,8 @@ class AdwoTest:
 
     base_url = "http://dsp.adwo.com/AdwoRTBBidder/BidProcesser"
 
-    def request_ads(self):
-        ''' 请求成功 '''
-        loop = 10
+    def request(self):
+        loop = 1000
         for i in range(0, loop):
             base_dir = str(os.path.dirname(os.path.dirname(__file__)))
             file_path = base_dir + "/config/request_640100_success.json"
@@ -21,37 +20,25 @@ class AdwoTest:
             f['id'] += str(i)
             f['imp'][0]['id'] += str(i)
             f['imp'][0]['banner']['id'] += str(i)
+            f['app']['id'] += str(i)
             f['device']['ext']['idfa'] += str(i)
             f['device']['ext']['udi']['idfa'] += str(i)
             print(json.dumps(f))
             headers = {'Content-Type': 'application/json'}
             r = requests.post(self.base_url, data=json.dumps(f), headers=headers)
             self.result = r.json()
-
-
-
-    def request_nurl(self):
-        nurl = self.result['seatbid'][0]['bid'][0]['nurl'].split('?')[0]
-        data = self.result['seatbid'][0]['bid'][0]['nurl'].split('?')[1]
-        r = requests.get(nurl, params=data)
-
-
-    def request_pm(self):
-        nurl = self.result['seatbid'][0]['bid'][0]['ext']['pm'][0].split('?')[0]
-        data = self.result['seatbid'][0]['bid'][0]['ext']['pm'][0].split('?')[1]
-        r = requests.get(nurl, params=data)
-
-
-    def request_cm(self):
-        self.request_ads()
-        self.request_nurl()
-        self.request_pm()
-        nurl = self.result['seatbid'][0]['bid'][0]['ext']['cm'][0].split('?')[0]
-        data = self.result['seatbid'][0]['bid'][0]['ext']['cm'][0].split('?')[1]
-        r = requests.get(nurl, params=data)
-        print(r)
+            nurl = self.result['seatbid'][0]['bid'][0]['nurl'].split('?')[0]
+            data = self.result['seatbid'][0]['bid'][0]['nurl'].split('?')[1]
+            r = requests.get(nurl, params=data)
+            nurl = self.result['seatbid'][0]['bid'][0]['ext']['pm'][0].split('?')[0]
+            data = self.result['seatbid'][0]['bid'][0]['ext']['pm'][0].split('?')[1]
+            u = requests.get(nurl, params=data)
+            nurl = self.result['seatbid'][0]['bid'][0]['ext']['cm'][0].split('?')[0]
+            data = self.result['seatbid'][0]['bid'][0]['ext']['cm'][0].split('?')[1]
+            v = requests.get(nurl, params=data)
+            print(v)
 
 if __name__ == '__main__':
-    AdwoTest().request_cm()
+    AdwoTest().request()
 
 
